1b625b7713d9be641fc174459e54ae2716bc62f5
Spinogris <vladislav77788@gmail.com>
2023-09-05 11:33:48 +0200

    Commit_05_09_23

---
+++ JavaBasic/Lesson_01_09_23_Summery_Game_BUTTLE/src/main/java/org/example/Superhero.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Random;
+
+public class Superhero {
+  public static void main(String[] args) {
+
+    String[] name = {"Subziro", "Shrek", "Donatello", "Mario", "Nemo"};
+    Random random = new Random();
+    String randomName = name[random.nextInt(name.length)];
+    System.out.println(randomName);
+    int health = 100;
+    int[] force = new int[5];
+    public Weapon generateRandomWeapon () {
+      String[] weapon = {"Sword", "Apple", "Holy grenade", "Sheep", "Finger"};
+      int[] force = {20, 10, 41, 5, 3};
+      int number = name.length;
+      Random random = new Random();
+      int random1 = random.nextInt(random);
+
+      return;
+    }
+    public void generateRandomSuperHero () {
+
+    }
+
+
+  }
+}
